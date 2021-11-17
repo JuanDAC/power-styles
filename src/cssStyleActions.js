e0914ffaad1974179a2dd52b9cd0704fd68637a9
JuanDAC <juandac@pop-os.localdomain>
2021-11-16 22:33:05 -0500

    [🚑️] (add dash-case for change the rules of css)

--- src/cssStyleActions.js
+++ src/cssStyleActions.js
@@ -1,10 +1,12 @@
 import { stylePropertyFactory } from "./core";
 import { varAdding } from "./handers";
 import uppercamelcase from "uppercamelcase";
+import { paramCase } from "param-case";
+
 
 export const styleActions = Object.fromEntries(
   Object.keys(document.body.style).map((property) => [
     uppercamelcase(property),
-    stylePropertyFactory(property, varAdding)
+    stylePropertyFactory(paramCase(property), varAdding)
   ]) // Object.keys(document.body.style).map(key => [
 ); // Object.fromEntries
