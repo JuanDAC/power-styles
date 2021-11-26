f33b73154c6f88b3f6f15a6a78a649f8c91c0011
JuanDAC <juanavila12.0308@gmail.com>
2021-11-26 06:52:33 -0500

    [➖] (remove getCustomProperties methods)

--- src/getterProperties.js
+++ src/getterProperties.js
@@ -7,11 +7,11 @@ export const getStyleDeclaration = () => STYLE_DECLARATIONS;
 
 /**
  * getCustoms:: void -> [string]
-export const getCustoms = () => {
-    const currentCustoms = getCustomProperties("power-styles")
-        .concat(getCustomPropertiesRegisted("#power-styles")
-            .map(({ name }) => name));
-
-    return currentCustoms.filter((value, index) => currentCustoms.indexOf(value) === index);
-}
+ * export const getCustoms = () => {
+ *   const currentCustoms = getCustomProperties("power-styles")
+ *       .concat(getCustomPropertiesRegisted("#power-styles")
+ *           .map(({ name }) => name));
+ *
+ *   return currentCustoms.filter((value, index) => currentCustoms.indexOf(value) === index);
+ *}
  */
